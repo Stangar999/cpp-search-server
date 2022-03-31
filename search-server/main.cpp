4c5d3064c60b9197ca8d2b1bc83e71f07f9fb503
Kirill <lockinout@mail.ru>
2022-04-01 01:00:36 +0300

    add log_duration, MatchDocuments, FindTopDocuments

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,14 +1,55 @@
+#include <iostream>
+
 #include "search_server.h"
 #include "string_processing.h"
 #include "request_queue.h"
 #include "paginator.h"
 #include "read_input_functions.h"
 #include "tests.h"
+#include "log_duration.h"
 
 using namespace std;
+//void AddDocument(SearchServer& search_server, int document_id, const string& document, DocumentStatus status,
+//                 const vector<int>& ratings) {
+//    try {
+//        search_server.AddDocument(document_id, document, status, ratings);
+//    } catch (const exception& e) {
+//        cout << "?????? ?????????? ????????? "s << document_id << ": "s << e.what() << endl;
+//    }
+//}
+
+void FindTopDocuments(const SearchServer& search_server, const string& raw_query) {
+    try {
+        LOG_DURATION_STREAM("Operation time", cout);
+        cout << "FindTopDocuments query: "s << raw_query << endl;
+        for (const Document& document : search_server.FindTopDocuments(raw_query)) {
+            PrintDocument(document);
+        }
+    } catch (const exception& e) {
+        cout << "FindTopDocuments FAIL: "s << e.what() << endl;
+    }
+}
+
+void MatchDocuments(const SearchServer& search_server, const string& query) {
+    try {
+        LOG_DURATION_STREAM("Operation time", cout);
+        cout << "MatchDocuments query: "s << query << endl;
+        const int document_count = search_server.GetDocumentCount();
+        for (int index = 0; index < document_count; ++index) {
+            const int document_id = search_server.GetDocumentId(index);
+            const auto [words, status] = search_server.MatchDocument(query, document_id);
+            PrintMatchDocumentResult(document_id, words, status);
+        }
+    } catch (const exception& e) {
+        cout << "MatchDocuments FAIL: "s << query << ": "s << e.what() << endl;
+    }
+}
 //-------------------------------------------------------------------------------------------------------------
 int main() {
-    TestSearchServer();
+    {
+      LOG_DURATION_STREAM("Operation time", cout);
+      TestSearchServer();
+    }
 
     SearchServer search_server("and in at"s);
     RequestQueue request_queue(search_server);
@@ -30,6 +71,9 @@ int main() {
     // первый запрос удален, 1437 запросов с нулевым результатом
     request_queue.AddFindRequest("sparrow"s);
     std::cout << "Total empty requests: "s << request_queue.GetNoResultRequests() << std::endl;
+
+    MatchDocuments(search_server, "curly cat"s);
+    FindTopDocuments(search_server, "big dog"s);
     return 0;
 }
 //-------------------------------------------------------------------------------------------------------------
